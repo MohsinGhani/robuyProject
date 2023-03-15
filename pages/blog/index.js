7a1e84f5dd7464379ac34d92a38909196aac1fc1
Mohsin Ghani <Mohsinghani.777@gmail.com>
2023-03-15 19:05:49 +0500

    complete blog page

---
+++ pages/blog/index.js
@@ -0,0 +1,12 @@
+import RobuySecondPage from "@/component/blog";
+import React from "react";
+
+const Index = () => {
+  return (
+    <div className="Index">
+      <RobuySecondPage />
+    </div>
+  );
+};
+
+export default Index;
--- public/assets/images/index.js
+++ public/assets/images/index.js
@@ -1,9 +1,13 @@
 import milto from "./milto.png";
 import dragon from "./dragon.png";
 import Roblox from "./Roblox.png";
+import rolboxpapa from "./rolboxpapa.png";
+import cambro from "./cambro.png";
 
 export default {
   milto,
   dragon,
   Roblox,
+  rolboxpapa,
+  cambro,
 };
