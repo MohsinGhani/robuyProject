25629bf803d1318c009ded6470b88b9d056176e2
Mohsin Ghani <Mohsinghani.777@gmail.com>
2023-03-16 21:08:01 +0500

    minor changes

--- public/assets/images/index.js
+++ public/assets/images/index.js
@@ -5,12 +5,12 @@ import Vector from "./Vector.png";
 import logo from "./logo.png";
 import twiter from "./twiter.png";
 import coin from "./coin.png";
-import union from "./Union.png";
+import union from "./union.png";
 import subtract from "./subtract.png";
 import vector3 from "./vector3.png";
 import union2 from "./union2.png";
 
-import rolboxpapa from "./rolboxpapa.png";
+import rolboxpapa from "./rolboxPapa.png";
 import cambro from "./cambro.png";
 
 export default {
